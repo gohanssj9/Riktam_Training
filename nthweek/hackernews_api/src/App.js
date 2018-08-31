c2d0fa66eaf33b3a310cd93606b90d9b467e5e45
gohanssj9 <sritejaayyagari01@gmail.com>
2018-08-31 12:15:06 +0530

    Client side Caching added.

--- nthweek/hackernews_api/src/App.js
+++ nthweek/hackernews_api/src/App.js
@@ -70,9 +70,12 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
     };
+
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
@@ -80,19 +83,26 @@ class App extends Component {
     this.onDismiss = this.onDismiss.bind(this);
   }
 
+  needsToSearchTopStories(searchTerm){
+    return !this.state.results[searchTerm];
+  }
+
   setSearchTopStories(result){
     const {hits, page} = result;
-
-    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const {searchKey, results} = this.state;
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
     const updatedHits = [...oldHits, ...hits];
 
-    this.setState({result: {hits: updatedHits, page}});
+    this.setState({results: { ...results, [searchKey]: {hits: updatedHits, page}}});
   }
 
   onDismiss(id){
+    const {searchKey, results} = this.state;
+    const {hits, page} = results[searchKey];
+
     const isNotId = item => item.objectID != id;
-    const updatedList = this.state.result.hits.filter(isNotId);
-    this.setState({result: {...this.state.result, hits: updatedList}});
+    const updatedList = hits.filter(isNotId);
+    this.setState({results: {...results, [searchKey]:{hits: updatedList, page}}});
   }
 
   onSearchChange(event){
@@ -101,7 +111,8 @@ class App extends Component {
 
   onSearchSubmit(event){
     const {searchTerm} = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({searchKey: searchTerm});
+    if(this.needsToSearchTopStories(searchTerm)) this.fetchSearchTopStories(searchTerm);
     event.preventDefault();
     // for real time hackernews api search
   }
@@ -115,18 +126,20 @@ class App extends Component {
 
   componentDidMount(){
     const { searchTerm } = this.state;
+    this.setState({searchKey: searchTerm});
     this.fetchSearchTopStories(searchTerm);
   }
 
 
   render() {
-    const {searchTerm, result} = this.state;
-    const page = (result && result.page) || 0;
+    const {searchTerm, results, searchKey} = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
     return (
       <div className="App" style = {allStyle}>
         <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}> Search </Search>
-        {result && <Table list={result.hits} onDismiss={this.onDismiss} />}
-        <Button className="btn-info btn-lg center-block" style={{ width: "30%"}} onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}> More </Button>
+        <Table list={list} onDismiss={this.onDismiss} />
+        <Button className="btn-info btn-lg center-block" style={{ width: "30%"}} onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}> More </Button>
       </div>
     );
   }
