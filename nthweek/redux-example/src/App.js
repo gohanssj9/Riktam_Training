fc84c8356f5cbe996d25eee24e7daf78de5aec3a
gohanssj9 <sritejaayyagari01@gmail.com>
2018-09-11 18:00:08 +0530

    Added Redux functionality

--- nthweek/redux-example/src/App.js
+++ nthweek/redux-example/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Provider} from 'react-redux';
+
+import store from './store';
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
@@ -9,15 +12,17 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
