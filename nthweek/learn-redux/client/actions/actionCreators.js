6ca5d7289f2bfb362ec7bdc89c0f3dd8996a4b0d
gohanssj9 <sritejaayyagari01@gmail.com>
2018-09-17 14:24:15 +0530

    Reduxstagram - WesBos Redux Course

---
+++ nthweek/learn-redux/client/actions/actionCreators.js
@@ -0,0 +1,25 @@
+// actions
+
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index: index
+  }
+}
+
+export function addComment(postId, author, comment){
+  return {
+    type: 'ADD_COMMENT',
+    postId: postId,
+    author: author,
+    comment: comment
+  }
+}
+
+export function removeComment(postId, commentId){
+  return {
+    type: 'REMOVE_COMMENT',
+    postId: postId,
+    commentId: commentId
+  }
+}
