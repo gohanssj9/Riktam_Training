121f2cf3e147c919cba45ec51728d6a25cd86125
gohanssj9 <sritejaayyagari01@gmail.com>
2018-10-05 11:12:49 +0530

    Before Responsive, v0.2

--- nthweek/ola-demo/src/components/Header.js
+++ nthweek/ola-demo/src/components/Header.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, MenuItem, NavItem, NavDropdown} from 'react-bootstrap';
 import '../assets/header.css';
 
 import HeaderPrimary from './HeaderPrimary';
--- nthweek/ola-demo/src/components/HeaderPrimary.js
+++ nthweek/ola-demo/src/components/HeaderPrimary.js
@@ -8,27 +8,27 @@ const HeaderPrimary = () => {
         <div className = "pull-right">
           <div className = "primary-links">
             <div className = "link-wrapper">
-              <a href="https://partners.olacabs.com/" target="_blank">Drive with Ola</a>
+              <a href="https://partners.olacabs.com/">Drive with Ola</a>
             </div>
 
             <div className="link-wrapper">
-              <a href="https://www.olamoney.com" target="_blank"> Ola Money</a>
+              <a href="https://www.olamoney.com"> Ola Money</a>
             </div>
 
             <div className="link-wrapper">
-              <a href="https://corporate.olacabs.com" target="_blank">Ola Corporate</a>
+              <a href="https://corporate.olacabs.com">Ola Corporate</a>
             </div>
 
             <div className="link-wrapper">
-              <a href="/share" target="_blank">Share</a>
+              <a href="/share">Share</a>
             </div>
 
             <div className="link-wrapper">
-              <a href="https://blog.olacabs.com/" target="_blank">Offers</a>
+              <a href="https://blog.olacabs.com/">Offers</a>
             </div>
 
             <div className="link-wrapper">
-              <a href="https://help.olacabs.com/support" target="_blank">Support</a>
+              <a href="https://help.olacabs.com/support">Support</a>
             </div>
           </div>
         </div>
